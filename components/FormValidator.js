e60dba005609b128e09403f9e759d30d4e858e63
SANDRA KEFFER <empathy7791@gmail.com>
2025-04-28 21:00:45 -0600

    project todo, first attempt

---
+++ components/FormValidator.js
@@ -0,0 +1,86 @@
+class FormValidator {
+  constructor(settings, formElement) {
+    this._inputSelector = settings.inputSelector;
+    this._submitButtonSelector = settings.submitButtonSelector;
+    this._errorClass = settings.errorClass;
+    this._inputErrorClass = settings.inputErrorClass;
+    this._inactiveButtonClass = settings.inactiveButtonClass;
+    this._formElement = formElement;
+  }
+
+  _showInputError(inputElement, errorMessage) {
+    const errorElementId = `#${inputElement.id}-error`;
+    const errorElement = this._formElement.querySelector(errorElementId);
+    inputElement.classList.add(this._inputErrorClass);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(this._errorClass);
+  }
+
+  _hideInputError(inputElement) {
+    const errorElementId = `#${inputElement.id}-error`;
+    const errorElement = this._formElement.querySelector(errorElementId);
+    inputElement.classList.remove(this._inputErrorClass);
+    errorElement.classList.remove(this._errorClass);
+    errorElement.textContent = "";
+  }
+
+  _checkInputValidity(inputElement) {
+    if (!inputElement.validity.valid) {
+      this._showInputError(inputElement, inputElement.validationMessage);
+    } else {
+      this._hideInputError(inputElement);
+    }
+  }
+
+  _hasInvalidInput() {
+    return this._inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    });
+  }
+
+  _toggleButtonState() {
+    const buttonElement = this._formElement.querySelector(
+      this._submitButtonSelector
+    );
+
+    if (this._hasInvalidInput()) {
+      buttonElement.disabled = true;
+      buttonElement.classList.add(this._inactiveButtonClass);
+    } else {
+      buttonElement.disabled = false;
+      buttonElement.classList.remove(this._inactiveButtonClass);
+    }
+  }
+
+  _setEventListeners() {
+    const inputList = this._formElement.querySelectorAll(this._inputSelector);
+    this._inputList = Array.from(inputList);
+
+    this._toggleButtonState();
+
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        this._checkInputValidity(inputElement);
+        this._toggleButtonState();
+      });
+    });
+  }
+
+  enableValidation() {
+    this._formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    this._setEventListeners();
+  }
+
+  resetValidation() {
+    const inputList = this._formElement.querySelectorAll(this._inputSelector);
+    inputList.forEach((inputElement) => {
+      this._hideInputError(inputElement);
+    });
+    this._formElement.reset();
+    this._toggleButtonState();
+  }
+}
+
+export default FormValidator;
---
+++ components/Todo.js
@@ -0,0 +1,57 @@
+class Todo {
+  constructor(data, templateSelector) {
+    this._data = data;
+    this._name = data.name;
+    this._date = data.date || null;
+    this._completed = data.completed || false;
+    this._id = data.id;
+    this._templateSelector = templateSelector;
+  }
+
+  _setEventListeners() {
+    this._todoCheckboxEl.addEventListener("change", () => {
+      this._data.completed = !this._data.completed;
+    });
+    this._todoDeleteBtn.addEventListener("click", () => {
+      this._view.remove();
+    });
+  }
+
+  setupCheckboxEl() {
+    this._todoCheckboxEl = this._view.querySelector(".todo__completed");
+    this._todoLabel = this._view.querySelector(".todo__label");
+    this._todoCheckboxEl.checked = this._data.completed;
+    this._todoCheckboxEl.id = `todo-${this._data.id}`;
+    this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
+  }
+
+  getView() {
+    this._templateElement = document.querySelector(this._templateSelector);
+    this._view = this._templateElement.content
+      .cloneNode(true)
+      .querySelector(".todo");
+
+    const todoNameEl = this._view.querySelector(".todo__name");
+    const todoDate = this._view.querySelector(".todo__date");
+    this._todoDeleteBtn = this._view.querySelector(".todo__delete-btn");
+
+    todoNameEl.textContent = this._data.name;
+    if (this._data.date) {
+      const dueDate = new Date(this._data.date);
+      if (!isNaN(dueDate)) {
+        todoDate.textContent = `Due: ${dueDate.toLocaleString("en-US", {
+          year: "numeric",
+          month: "short",
+          day: "numeric",
+        })}`;
+      }
+    }
+
+    this.setupCheckboxEl();
+    this._setEventListeners();
+
+    return this._view;
+  }
+}
+
+export default Todo;
